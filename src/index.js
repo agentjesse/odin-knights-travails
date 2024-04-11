8262b6302f9850995fc00665079a3eac4cecf015
jesse <31576321+agentjesse@users.noreply.github.com>
2024-04-11 10:12:05 -0400

    added some pseudocode

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 /* Next task:
--  Build a function mergeSort that takes in an array and returns a sorted array,
- using a recursive merge sort methodology. An input of [3,1,4,2] should return [1,2,3,4].
-*/
-/* pattern finding chart
+- make fns/factories:
+treeNode : { coordinates, parent=none }
+validKnightMoves(currentNode.coordinates): return arr of up to 8 valid move coordinates
+constructTree(startCoord, endCoord):
+  root = treeNode(startCoord)
+  make a queue
+  while queue occupied:
+    currentNode = dequeue a node
+    Generate valid knight moves from current coordinates and loop over them:
+    for move of validKnightMoves(currentNode.coordinates):
+      if move == end: RETURN currentNode
+      childNode = treeNode(move, parent=currentNode)
+      enqueue childNode
+  if no path found: RETURN null
+reconstructPath(endNode): returns a path []
+//driver script:
+endNode = constructTree(start_coord, end_coord)
+if endingNode: shortestPath = reconstructPath(endingNode)
+else: return "No valid path found."
 */
 
 //For Node.js, when importing local modules, include the file extension in the import statement.
